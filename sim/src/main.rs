13cd3314300cd3ca8eeff09b0a50bf6924f9773f
Jan Segre <jan@segre.in>
2016-05-09 15:45:08 -0300

    Base for GUI, based on glutin's triangle example.

---
+++ sim/src/main.rs
@@ -0,0 +1,186 @@
+extern crate roboime_next;
+extern crate roboime_next_sim;
+#[macro_use] extern crate glium;
+extern crate clock_ticks;
+
+use std::thread;
+use std::time::Duration;
+use glium::Surface;
+use glium::glutin;
+use glium::index::PrimitiveType;
+
+enum Action {
+    Stop,
+    Continue,
+}
+
+fn start_loop<F>(mut callback: F) where F: FnMut() -> Action {
+    let mut accumulator = 0;
+    let mut previous_clock = clock_ticks::precise_time_ns();
+
+    loop {
+        match callback() {
+            Action::Stop => break,
+            Action::Continue => ()
+        };
+
+        let now = clock_ticks::precise_time_ns();
+        accumulator += now - previous_clock;
+        previous_clock = now;
+
+        const FIXED_TIME_STAMP: u64 = 16666667;
+        while accumulator >= FIXED_TIME_STAMP {
+            accumulator -= FIXED_TIME_STAMP;
+
+            // if you have a game, update the state here
+        }
+
+        thread::sleep(Duration::from_millis(((FIXED_TIME_STAMP - accumulator) / 1000000) as u64));
+    }
+}
+
+fn main() {
+    use glium::DisplayBuild;
+
+    // building the display, ie. the main object
+    let display = glutin::WindowBuilder::new()
+        .build_glium()
+        .unwrap();
+
+    // building the vertex buffer, which contains all the vertices that we will draw
+    let vertex_buffer = {
+        #[derive(Copy, Clone)]
+        struct Vertex {
+            position: [f32; 2],
+            color: [f32; 3],
+        }
+
+        implement_vertex!(Vertex, position, color);
+
+        glium::VertexBuffer::new(&display,
+            &[
+                Vertex { position: [-0.5, -0.5], color: [0.0, 1.0, 0.0] },
+                Vertex { position: [ 0.0,  0.5], color: [0.0, 0.0, 1.0] },
+                Vertex { position: [ 0.5, -0.5], color: [1.0, 0.0, 0.0] },
+            ]
+        ).unwrap()
+    };
+
+    // building the index buffer
+    let index_buffer = glium::IndexBuffer::new(&display, PrimitiveType::TrianglesList,
+                                               &[0u16, 1, 2]).unwrap();
+
+    // compiling shaders and linking them together
+    let program = program!(&display,
+        140 => {
+            vertex: "
+                #version 140
+
+                uniform mat4 matrix;
+
+                in vec2 position;
+                in vec3 color;
+
+                out vec3 vColor;
+
+                void main() {
+                    gl_Position = vec4(position, 0.0, 1.0) * matrix;
+                    vColor = color;
+                }
+            ",
+
+            fragment: "
+                #version 140
+                in vec3 vColor;
+                out vec4 f_color;
+
+                void main() {
+                    f_color = vec4(vColor, 1.0);
+                }
+            "
+        },
+
+        110 => {
+            vertex: "
+                #version 110
+
+                uniform mat4 matrix;
+
+                attribute vec2 position;
+                attribute vec3 color;
+
+                varying vec3 vColor;
+
+                void main() {
+                    gl_Position = vec4(position, 0.0, 1.0) * matrix;
+                    vColor = color;
+                }
+            ",
+
+            fragment: "
+                #version 110
+                varying vec3 vColor;
+
+                void main() {
+                    gl_FragColor = vec4(vColor, 1.0);
+                }
+            ",
+        },
+
+        100 => {
+            vertex: "
+                #version 100
+
+                uniform lowp mat4 matrix;
+
+                attribute lowp vec2 position;
+                attribute lowp vec3 color;
+
+                varying lowp vec3 vColor;
+
+                void main() {
+                    gl_Position = vec4(position, 0.0, 1.0) * matrix;
+                    vColor = color;
+                }
+            ",
+
+            fragment: "
+                #version 100
+                varying lowp vec3 vColor;
+
+                void main() {
+                    gl_FragColor = vec4(vColor, 1.0);
+                }
+            ",
+        },
+    ).unwrap();
+
+    // the main loop
+    start_loop(|| {
+        // building the uniforms
+        let uniforms = uniform! {
+            matrix: [
+                [1.0, 0.0, 0.0, 0.0],
+                [0.0, 1.0, 0.0, 0.0],
+                [0.0, 0.0, 1.0, 0.0],
+                [0.0, 0.0, 0.0, 1.0f32]
+            ]
+        };
+
+        // drawing a frame
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 0.0, 0.0);
+        target.draw(&vertex_buffer, &index_buffer, &program, &uniforms, &Default::default()).unwrap();
+        target.finish().unwrap();
+
+        // polling and handling the events received by the window
+        for event in display.poll_events() {
+            match event {
+                glutin::Event::Closed => return Action::Stop,
+                _ => ()
+            }
+        }
+
+        Action::Continue
+    });
+}
