e26da2b7e1e4f5b3267f2f7db06d9c6618aef66d
Jan Segre <jan@segre.in>
2016-05-08 23:44:01 -0300

    Refactor cpp demo.

--- cli/demos/cpp/main.cpp
+++ cli/demos/cpp/main.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
-#include <stdio.h>
 #include <fstream>
 #include <vector>
-#include <math.h>
-
-const double PI = 3.14159265;
+#include <cmath>
 
 int main() {
     using namespace std;
 
-    //Version check I/O
+    cerr << "started" << endl;
+
+    // Version check I/O
 
     char protocol_version_string[32];
     int version;
@@ -17,12 +16,12 @@ int main() {
     cin >> version;
     if (version == 1) {
         cout << "COMPATIBLE 1" << endl;
-    }
-    else {
+    } else {
         cout << "NOT_COMPATIBLE 1" << endl;
     }
+    cerr << "compatible" << endl;
 
-    //Geometry input
+    // Geometry input
 
     float field_length;
     float field_width;
@@ -44,26 +43,28 @@ int main() {
         >> penalty_spot_from_field_line_dist
         >> penalty_line_from_spot_dist;
 
-    //Game state I/O
-    int counter;
-    float timestamp;
-    char referee_state;
-    float referee_time_left;
-    int score_player;
-    int score_opponent;
-    int goalkeeper_id_player;
-    int goalkeeper_id_opponent;
-    int robot_num_player;
-    int robot_num_opponent;
-    float ball_x, ball_y, ball_vx, ball_vy;
+    cerr << "initialized" << endl;
 
+    // Game state I/O
     while (true) {
+
         // State
         vector<int> identifiers;
         float x = 0.0f, y = 0.0f, w = 0.0f;
         float tx = 0.0f, ty = 0.0f, tw = 0.0f;
 
         // Input
+        int counter;
+        float timestamp;
+        char referee_state;
+        float referee_time_left;
+        int score_player;
+        int score_opponent;
+        int goalkeeper_id_player;
+        int goalkeeper_id_opponent;
+        int robot_num_player;
+        int robot_num_opponent;
+        float ball_x, ball_y, ball_vx, ball_vy;
 
         cin >> counter
             >> timestamp
@@ -79,6 +80,7 @@ int main() {
         for (int i = 0; i < robot_num_player; ++i) {
             int robot_id;
             float robot_x, robot_y, robot_w, robot_vx, robot_vy, robot_vw;
+
             cin >> robot_id >> robot_x >> robot_y >> robot_w >> robot_vx >> robot_vy >> robot_vw;
             identifiers.push_back(robot_id);
             if (robot_id == 0) {
@@ -91,32 +93,39 @@ int main() {
         for (int i = 0; i < robot_num_opponent; ++i) {
             int robot_id;
             float robot_x, robot_y, robot_w, robot_vx, robot_vy, robot_vw;
+
             cin >> robot_id >> robot_x >> robot_y >> robot_w >> robot_vx >> robot_vy >> robot_vw;
         }
+
         tx = ball_x;
         ty = ball_y;
         tw = 0;
+
         cout << counter << endl;
-        const float p_t = 0.4f;
-        const float p_w = 0.8000f;
+
+        const float p_t = 0.40f;
+        const float p_w = 0.80f;
         for (int i = 0; i < identifiers.size() ; ++i) {
+            float v_tan, v_norm, v_ang, kick_x, kick_z;
+            bool spin;
+
             if (identifiers.at(i) == 0) {
-                const float v_tan = p_t * ((tx - x) * cos(w * PI/180.0) + (ty - y) * sin(w * PI / 180.0));
-                const float v_norm = p_t * ((ty - y) * cos(w * PI/180.0) + (tx - x) * sin(w * PI / 180.0));
-                const float v_ang = p_w*(tw-w);
-                const float kick_x = 4.0f;
-                const float kick_z = 0.0f;
-                const bool spin = true;
-                cout << v_tan << " " << v_norm << " " << v_ang << " " << kick_x << " " << kick_z << " " << spin << endl;
+                v_tan = p_t * ((tx - x) * cos(w) + (ty - y) * sin(w));
+                v_norm = p_t * ((ty - y) * cos(w) + (tx - x) * sin(w));
+                v_ang = p_w * (tw - w);
+                kick_x = 4.0f;
+                kick_z = 0.0f;
+                spin = true;
             } else {
-                const float v_tan = 0.0f;
-                const float v_norm = 0.0f;
-                const float v_ang = 0.0f;
-                const float kick_x = 0.0f;
-                const float kick_z = 0.0f;
-                const bool spin = false;
-                cout << v_tan << " " << v_norm << " " << v_ang << " " << kick_x << " " << kick_z << " " << spin << endl;
+                v_tan = 0.0f;
+                v_norm = 0.0f;
+                v_ang = 0.0f;
+                kick_x = 0.0f;
+                kick_z = 0.0f;
+                spin = false;
             }
+
+            cout << v_tan << " " << v_norm << " " << v_ang << " " << kick_x << " " << kick_z << " " << spin << endl;
         }
     }
 }
