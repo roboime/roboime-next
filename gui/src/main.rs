54c37aeefa802a23e3d15846e99b30b090e03dbe
Jan Segre <jan@segre.in>
2016-05-18 14:47:33 -0300

    Refactor rendering components.

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -15,6 +15,7 @@ use ::models::TeamSide;
 pub mod models;
 pub mod colors;
 mod utils;
+mod draw;
 
 fn main() {
     use std::thread;
@@ -36,109 +37,20 @@ fn main() {
         .build_glium()
         .unwrap();
 
-    // compiling shaders and linking them together
-    let program = program!(&display,
-        150 => { vertex: include_str!("vertex.glsl"), fragment: include_str!("fragment.glsl"), outputs_srgb: true },
-    ).unwrap();
-
-    let simple_program = program!(&display,
-        150 => {
-            vertex: r#"
-                #version 150
-
-                uniform mat4 perspective;
-                uniform mat4 view;
-                uniform mat4 model;
-                in vec3 position;
-                in vec3 color;
-                out vec3 v_color;
-
-                void main() {
-                    mat4 modelview = view * model;
-                    gl_Position = perspective * modelview * vec4(position, 1.0);
-                    v_color = color;
-                }
-            "#,
-
-            fragment: r#"
-                #version 150
-
-                in vec3 v_color;
-                out vec4 f_color;
-                //const float gamma = 1.5;
-
-                void main() {
-                    //vec3 c_color = v_color * 0.9;
-                    //vec3 color = vec3(pow(c_color.x, gamma), pow(c_color.y, gamma), pow(c_color.z, gamma));
-                    //f_color = vec4(color, 1.0);
-                    f_color = vec4(v_color, 1.0);
-                }
-            "#,
-
-            outputs_srgb: true
-        },
-    ).unwrap();
-
-    let params = glium::DrawParameters {
-        depth: glium::Depth {
-            test: glium::draw_parameters::DepthTest::IfLessOrEqual,
-            write: true,
-            .. Default::default()
-        },
-        backface_culling: glium::draw_parameters::BackfaceCullingMode::CullCounterClockwise,
-        .. Default::default()
-    };
-
-    let params_pre = glium::DrawParameters {
-        depth: glium::Depth {
-            write: true,
-            .. Default::default()
-        },
-        .. params.clone()
-    };
-
-    //let light = [-1.0, 0.4, -0.9f32];
-    let light = [-0.2, 2.0, -1.0f32];
-    let bg_color = { let (r, g, b) = colors::DARK_GREEN; (r, g, b, 1.0) };
-
     let team_side = TeamSide::BlueIsLeft;
-    let field = models::field(&display, team_side);
-    let goals = models::goals(&display, team_side);
-    let ball  = models::ball(&display);
-    let (yellow_robots, blue_robots) = {
-        use std::collections::HashMap;
-
-        let (n, m) = (17, 11);
-
-        let mut robots_y = HashMap::with_capacity(12);
-        for i in 0..12 {
-            let robot = models::robot(&display, i, true, n, m);
-            robots_y.insert(i, robot);
-        }
-
-        let mut robots_b = HashMap::with_capacity(12);
-        for i in 0..12 {
-            let robot = models::robot(&display, i, false, n, m);
-            robots_b.insert(i, robot);
-        }
-
-        (robots_y, robots_b)
-    };
-
-    //let normal_map = {
-    //    let image = image::load(Cursor::new(&include_bytes!("bumps.png")[..]), image::PNG).unwrap().to_rgba();
-    //    let image_dimensions = image.dimensions();
-    //    let image = glium::texture::RawImage2d::from_raw_rgba_reversed(image.into_raw(), image_dimensions);
-    //    glium::texture::Texture2d::new(&display, image).unwrap()
-    //};
+    let mut draw_game = draw::Game::new(&display).unwrap();
+    draw_game.team_side(&display, team_side);
 
     let mut accumulator = 0;
     let mut previous_clock = clock_ticks::precise_time_ns();
-    let mut game_state = game::State::new();
+    let game_state = game::SharedState::new();
 
     // add some robots
-    add_initial_robots(game_state.get_robots_blue_mut(), 6, false);
-    add_initial_robots(game_state.get_robots_yellow_mut(), 6, true);
+    {
+        let mut game_state = game_state.write().unwrap();
+        add_initial_robots(game_state.get_robots_blue_mut(), 6, false);
+        add_initial_robots(game_state.get_robots_yellow_mut(), 6, true);
+    }
 
     // the main loop
     'main: loop {
@@ -155,61 +67,9 @@ fn main() {
         //let view = view_matrix(&[4.0, 0.7, 1.6], &[0.2, -0.2, -0.6], &[0.0, 0.0, 1.0]);
         //let view = view_matrix(&[5.0, -0.7, 1.6], &[-0.2, 0.2, -0.6], &[0.0, 0.0, 1.0]);
 
-        let perspective = {
-            let (width, height) = target.get_dimensions();
-            perspective_matrix(width as f32, height as f32)
-        };
-
-        target.clear_color_srgb_and_depth(bg_color, 1.0);
-
-        target.draw(&field.0, &field.1, &simple_program, &uniform! {
-            model: [
-                [1.0, 0.0, 0.0, 0.0],
-                [0.0, 1.0, 0.0, 0.0],
-                [0.0, 0.0, 1.0, 0.0],
-                [0.0, 0.0, 0.0, 1.0f32]
-            ],
-            view: view,
-            perspective: perspective,
-        }, &params_pre).unwrap();
-
-        target.draw(&goals.0, &goals.1, &program, &uniform! {
-            model: [
-                [1.0, 0.0, 0.0, 0.0],
-                [0.0, 1.0, 0.0, 0.0],
-                [0.0, 0.0, 1.0, 0.0],
-                [0.0, 0.0, 0.0, 1.0f32]
-            ],
-            view: view,
-            perspective: perspective,
-            u_light: light,
-        }, &params).unwrap();
-
-        target.draw(&ball.0, &ball.1, &program, &uniform! {
-            model: game_state.get_ball().model_matrix(),
-            view: view,
-            perspective: perspective,
-            u_light: light,
-        }, &params).unwrap();
-
-        for (robot_id, robot_state) in game_state.get_robots_blue() {
-            let ref robot = blue_robots[robot_id];
-            target.draw(&robot.0, &robot.1, &program, &uniform! {
-                model: robot_state.model_matrix(),
-                view: view,
-                perspective: perspective,
-                u_light: light,
-            }, &params).unwrap();
-        }
-
-        for (robot_id, robot_state) in game_state.get_robots_yellow() {
-            let ref robot = yellow_robots[robot_id];
-            target.draw(&robot.0, &robot.1, &program, &uniform! {
-                model: robot_state.model_matrix(),
-                view: view,
-                perspective: perspective,
-                u_light: light,
-            }, &params).unwrap();
+        {
+            let state = game_state.read().unwrap();
+            draw_game.draw_to(&mut target, &state, view).unwrap();
         }
 
         target.finish().unwrap();
